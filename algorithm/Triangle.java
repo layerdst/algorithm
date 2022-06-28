dc484e5c95f8cdbd456257580fe74df936e73d81
layerdst <bccfcc@hanmail.net>
2022-06-28 23:54:51 +0900

    algorithm basic

---
+++ algorithm/Triangle.java
@@ -0,0 +1,69 @@
+package algorithm;
+
+public class Triangle {
+    static void triangleLB(int a){
+        for(int i=0;i<a;i++){
+            System.out.println();
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+        }
+    }
+
+    static void triangleLU(int a){
+        for(int i = a; i>0;i-- ){
+            System.out.println();
+            for(int j=i;j>0;j--){
+                System.out.print("*");
+            }
+        }
+    }
+
+    static void triangleRU(int a ){
+        for(int i = 0; i<a;i++ ){
+            System.out.println();
+            for(int j=0;j<a;j++){
+                if(i>j){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+        }
+    }
+
+    static void triangleRB(int a ){
+        for(int i = 0; i<a;i++ ){
+            System.out.println();
+            for(int j=0;j<a;j++){
+                if(a-i-1>j){
+                    System.out.print(" ");
+                }else{
+                    System.out.print("*");
+                }
+            }
+        }
+    }
+
+    static void pyramid(int a){
+        for(int i = 0 ; i<a;i++){
+            System.out.println();
+            for(int j=1;j<=2*a-1;j++){
+                System.out.print(" ");
+                if(j==a/2+1){
+                    System.out.print(j);
+                }else if(j>=a/2+1-i && j<=a/2+1+i){
+                    System.out.print(j);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        triangleLB(5);
+        triangleLU(5);
+        triangleRU(5);
+        triangleRB(5);
+        pyramid(4);
+    }
+}
