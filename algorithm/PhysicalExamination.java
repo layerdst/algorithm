569cf33eecaa016b7ed29db5b4b75eac9f26b845
layerdst <bccfcc@hanmail.net>
2022-07-03 22:56:57 +0900

    INSTANCE GET DATA

---
+++ algorithm/PhysicalExamination.java
@@ -0,0 +1,79 @@
+package algorithm;
+
+public class PhysicalExamination {
+
+    static final int VMAX = 21;
+
+    static class PhycsData{
+        String name;
+        int height;
+        double vision;
+
+        public PhycsData(String name, int height, double vision) {
+            this.name = name;
+            this.height = height;
+            this.vision = vision;
+        }
+    }
+
+    static double avgHeight(PhycsData[] data){
+        double sum = 0;
+
+        for(PhycsData temp : data){
+            sum += temp.height;
+        }
+
+        return sum / data.length;
+    }
+
+    static void distVision(PhycsData[] data, int[] dist) {
+        int i = 0;
+        dist[i] =0;
+        for (PhycsData temp : data) {
+            if (temp.vision >= 0.0 && temp.vision < VMAX / 10.0) {
+                dist[(int) (temp.vision * 10)]++;
+            }
+        }
+    }
+
+    static void distVisionStar(PhycsData[] data, int[] dist) {
+        int i = 0;
+        dist[i] =0;
+        for (PhycsData temp : data) {
+            if (temp.vision >= 0.0 && temp.vision < VMAX / 10.0) {
+                dist[(int) (temp.vision * 10)]++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        PhycsData[] x = {
+                new PhycsData("김", 125, 0.3),
+                new PhycsData("이", 150, 0.2),
+                new PhycsData("최", 170, 0.6),
+                new PhycsData("긴", 168, 0.5),
+                new PhycsData("일", 180, 0.4),
+                new PhycsData("선", 190, 0.4),
+        };
+
+        int[] vdist = new int[VMAX];
+
+        System.out.println(avgHeight(x));
+        distVision(x,vdist);
+
+        for (int d =0; d<vdist.length;d++ ) {
+            if(vdist[d]==0) continue;
+            String temp = "";
+            System.out.print((float) (d) / 10);
+            for(int a =0; a<vdist[d];a++){
+                temp += "*";
+            }
+            System.out.print(" : " + temp);
+            System.out.println();
+        }
+
+    }
+
+
+
+}
--- algorithm/PrimeNum.java
+++ algorithm/PrimeNum.java
@@ -2,6 +2,10 @@ package algorithm;
 
 import java.util.Scanner;
 
+/*
+* 소수 구하기
+* 2부터 입력한 값을 포함한 수까지 나눈다.
+* */
 public class PrimeNum {
     int count;
     static void primeNumPrint(int n){
@@ -18,20 +22,27 @@ public class PrimeNum {
         }
     }
 
+
+/*
+* Development 1 소수구하기
+* 2부터 소수를 구한다.
+* 단, 2 부터 소수는 홀수에 존재하므로 +=2 씩 값을 증가시킨다.
+* 소수는 배열에 저장하고, 저장된 소수로 나눗셈을 진행한다.
+*/
+
     static void newPrimeNumber(int n){
         int[] tempArr = new int[200];
         int idx = 0;
         tempArr[idx++] = 2;
 
-        if(n==2){
+        if(n%2==0){
             System.out.println("2");
-            return;
         }
 
         for(int i =3 ;i<=n;i+=2){
             int j;
             for (j = 1; j < idx; j++) {
-                if(i%tempArr[idx]==0){
+                if(i%tempArr[j]==0){
                     break;
                 }
             }
@@ -43,16 +54,69 @@ public class PrimeNum {
         }
     }
 
+    /*
+     * Development 2 소수구하기
+     * 모든 수는 제곱근을 기준으로 작은수와 큰수의 곱으로 이루어진다
+     * 나누는 수의 제곱이 입력값을 넘지 않을때 까지 나눗셈을 반복한다.
+     * 소수는 배열에 저장하고, 저장된 소수로 나눗셈을 진행한다.
+     */
+
+    static void squareRootPrimeNum(int n){
+        int tempArr[] = new int[100];
+        int idx = 0;
+
+        tempArr[idx++] = 2;
+        tempArr[idx++] = 3;
+
+        for (int i = 5; i < n; i += 2) {
+           boolean flag = false;
+           for(int j =1; tempArr[j] * tempArr[j]<=i;j++){
+               if(i%tempArr[j]==0){
+                   flag = true;
+                   break;
+               }
+               if(!flag){
+                   tempArr[idx++] = i;
+               }
+           }
+        }
+
+    }
+
+    static void squareRootPrimeNumber(int n){
+        int[] tempArr = new int[500];
+        int idx = 0;
+
+        tempArr[idx++] =2;
+        tempArr[idx++]= 3;
+
+        for (int i = 5; i < n; i += 2) {
+            boolean flag = false;
+            for (int j = 1; tempArr[j] * tempArr[j] <= i; j++) {
+                if (i % tempArr[j]==0) {
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag){
+                tempArr[idx++] = i;
+                System.out.println(i);
+            }
+        }
+
+    }
+
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1부터 해당 범위 까지 나열할 소수를 나열하시오");
-        int num;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("1부터 해당 범위 까지 나열할 소수를 나열하시오");
+//        int num;
+//
+//        do {
+//            num = sc.nextInt();
+//        } while (num < 2);
 
-        do {
-            num = sc.nextInt();
-        } while (num < 2);
-
-        newPrimeNumber(num);
+        squareRootPrimeNumber(100);
 
 
     }
