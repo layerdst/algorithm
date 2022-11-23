670247b7751123f66a6599fb7924fdf05f45f0d0
layerdst <71206860+layerdst@users.noreply.github.com>
2022-11-23 23:51:02 +0900

    Update Max.java

--- algorithm/Max.java
+++ algorithm/Max.java
@@ -1,9 +1,19 @@
 public class Max {
-    public int solution(int[] numbers) {
+    public int GreateMultiply(int[] numbers) {
 
         int[] temp = Arrays.stream(numbers).sorted().toArray();
         int len = temp.length;
 
         return temp[len-1]*temp[len-2];
     }
+    
+    public int maxValue(int a, int b, int c, int d){
+        int max=a;
+        if(max<b){max=b;}
+        if(max<c){max=c;}
+        if(max<d){max=d;}
+        return max;
+    }
+    
+    
 }
