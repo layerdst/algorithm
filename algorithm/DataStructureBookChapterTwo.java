4bd0aef7de33fb67d255c2daf4c6e7f034544ab2
layerdst <71206860+layerdst@users.noreply.github.com>
2022-11-29 16:36:36 +0900

    Update DataStructureBookChapterTwo.java

--- algorithm/DataStructureBookChapterTwo.java
+++ algorithm/DataStructureBookChapterTwo.java
@@ -31,6 +31,72 @@ public class ExampleChapterTwo {
             }
         }
     }
+    
+    static class YMD {
+        int y;
+        int m;
+        int d;
+
+        public YMD(int y, int m, int d) {
+            setYear(y);
+            setMonth(m);
+            setDay(d);
+        }
+
+        public enum Months{
+            THIRTYONE,
+            THIRTY,
+            LEAP
+        }
+
+
+        void setYear(int year){
+            if (year >= 0) {
+                this.y = year;
+            }else {
+                throw new IllegalArgumentException("년도는 1년 부터입니다.");
+            }
+        }
+
+        void setMonth(int month) {
+            if (month >= 1 && month <= 12) {
+                this.m = month;
+            }else {
+                throw new IllegalArgumentException("해당 월은 1에서 12월 까지 입니다.");
+            }
+        }
+
+        void setDay(int day) {
+            switch (m){
+                case 1: case 3: case 5: case 7 : setDay(day, 31); break;
+                case 2: setLeapDay(day); break;
+                default: setDay(day, 30);
+            }
+        }
+
+        void setLeapDay(int day) {
+            if(y%400 ==0 ){
+                setDay(day, 29);
+            } else if (y%100 == 0) {
+                setDay(day, 28);
+            } else if( y % 4 == 0){
+                setDay(day, 29);
+            } else {
+                setDay(day, 28);
+            }
+        }
+
+        void setDay(int day, int lastMonthDay){
+            if(boundedDay(day, lastMonthDay)){
+                this.d = day;}
+            else {
+                throw new IllegalArgumentException("해당 월의 일은 " + lastMonthDay +"까지 입니다.");
+            }
+        }
+
+        boolean boundedDay(int day, int lastMonthDay) {
+            return (day >= 1 && day <= lastMonthDay);
+        }
 
     public static void main(String[] args) {
         PhysicData[] x = {
@@ -54,6 +120,8 @@ public class ExampleChapterTwo {
         for(int i = 0 ; i < VMAX ; i++){
             System.out.println(i/10.0 + ": "+ "*".repeat(vdist[i]));
         }
+        
+        YMD ymd = new YMD(2004, 2, 30);
     }
 
 }
