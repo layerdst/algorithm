087b094002aa258f6936fb78d2f501f007da83d5
layerdst <71206860+layerdst@users.noreply.github.com>
2022-12-05 17:14:36 +0900

    Update DataStructureBookChapterThree.java

--- algorithm/DataStructureBookChapterThree.java
+++ algorithm/DataStructureBookChapterThree.java
@@ -104,6 +104,45 @@ public class ExampleChapterThree {
 
 
 
+    static class PhyscData{
+        private String name;
+        private int height;
+        private double vision;
+
+        public PhyscData(String name, int height, double vision) {
+            this.name = name;
+            this.height = height;
+            this.vision = vision;
+        }
+
+        @Override
+        public String toString() {
+            return "PhyscData{" +
+                    "name='" + name + '\'' +
+                    ", height=" + height +
+                    ", vision=" + vision +
+                    '}';
+        }
+
+        public static final Comparator<PhyscData> HEIGHT_ORDER = new HeightOrderComparator();
+        public static final Comparator<PhyscData> VISION_ORDER = new VisionOrderComparator();
+        private static class HeightOrderComparator implements Comparator<PhyscData>{
+            public int compare(PhyscData d1, PhyscData d2){
+                return(d1.height>d2.height) ? 1 : (d1.height < d2.height) ? -1 : 0;
+            }
+        }
+
+        public static class VisionOrderComparator implements Comparator<PhyscData>{
+
+            @Override
+            public int compare(PhyscData o1, PhyscData o2) {
+                return (o1.vision>o2.vision) ? 1 : (o1.vision<o2.vision) ? -1 : 0;
+            }
+        }
+    }
+
+
+
     public static void main(String[] args) {
 //        Scanner stdn = new Scanner(System.in);
 //
@@ -136,39 +175,26 @@ public class ExampleChapterThree {
         int[] arrA = {1,2,3,4,5,6,7,8};
         System.out.println(binarySearchGraph(arrA, 3));
 
-    }
+        Scanner stdIn = new Scanner(System.in);
+        PhyscData[] x = {
+                new PhyscData("강", 135, 0.3 ),
+                new PhyscData("김", 152, 0.4 ),
+                new PhyscData("이", 180, 0.5 ),
+                new PhyscData("최", 190, 0.7 ),
+                new PhyscData("백", 170, 1.0 ),
+        };
 
-    
-    
+        System.out.println("키가 몇이 사람");
+        int height = stdIn.nextInt();
+        int idx = Arrays.binarySearch(x, new PhyscData("", height, 0.0), PhyscData.HEIGHT_ORDER);
 
-    public static void main(String[] args) {
-//        Scanner stdn = new Scanner(System.in);
-//
-//        System.out.println("요소수");
-//
-//        int num = stdn.nextInt();
-//        int [] x = new int[num-1];
-//
-//        for(int i = 0; i<num; i++){
-//            System.out.println("x[" + i +"] : ");
-//            x[i] = stdn.nextInt();
-//        }
-//
-//        System.out.println("검색할값");
-//
-//        int key = stdn.nextInt();
-//
-//        int idx = seqSearchSen(x, num, key);
-//
-//        if(idx == -1){
-//            System.out.println("찾을 수 없음");
-//        }else{
-//            System.out.println("값은 " + idx + "번 째에 있다.");
-//        }
-
-        int[] arr = {1,2,3,4,5,6, 0};
-        seqSearchSenFor(arr, 6, 3);
+        if(idx < 0) {
+            System.out.println("그 값의 요소가 없습니다");
+        }else{
+            System.out.println("그 값은 " + idx + " 에 있음");
+        }
 
     }
+
 
 }
