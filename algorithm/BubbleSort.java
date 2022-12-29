ebf03fe06645d558a91a46b4c3d876747250a311
layerdst <71206860+layerdst@users.noreply.github.com>
2022-12-29 20:52:06 +0900

    Update BubbleSort.java

--- algorithm/BubbleSort.java
+++ algorithm/BubbleSort.java
@@ -1,21 +1,22 @@
-class BubbleSort{
-  static void swap(int [] a, int idx1, int idx2){
-    int temp = a[idx1];
-    a[idx1] = a[idx2];
-    a[idx2] = temp; 
-  }
-  
-  static void bubbleSort(int [] a, int n){
-    for(int i=0; i<n-1; i++){
-      for(int j = n-1; j>i;j--){
-        if(a[j-1] > a[j]){
-          swap(a, j-1, j);
-        }
-      }
-    }
-  }
-  
-  static void bubbleSort(int[] a, int n) {
+public class BubbleSort {
+
+	static void swap(int[] a, int idx1, int idx2) {
+		int temp = a[idx1];
+		a[idx1] = a[idx2];
+		a[idx2] = temp;
+	}
+
+	static void bubbleSort(int[] a, int n) {
+		for (int i = 0; i < n - 1; i++) {
+			for (int j = n - 1; j > i; j--) {
+				if (a[j - 1] > a[j]) {
+					swap(a, j - 1, j);
+				}
+			}
+		}
+	}
+
+	static void bubbleSort(int[] a, int n) {
 		int ccnt = 0;			// 비교 회수
 		int scnt = 0;			// 교환 회수
 
@@ -37,8 +38,9 @@ class BubbleSort{
 				System.out.printf("%3d  " , a[m]);
       			}
 			System.out.println();
-		} gh
+		} 
 		System.out.println("비교를 " + ccnt + "회 했습니다.");
 		System.out.println("교환을 " + scnt + "회 했습니다.");
 	}
+
 }
